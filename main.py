9c1db331d24e23f8a08034f85cdb836b843371c3
SudoCorentin <corentin.trebaol@hec.edu>
2022-11-13 08:50:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# https://replit.com/talk/learn/How-to-make-Rest-Api-in-Python/9038
+# Create webserver
+from flask import Flask
+from threading import Thread
+from flask_restful import Resource, Api
+from process import *
+
+app = Flask('')
+# Flask restful comes with an Api function that makes your life much better than with Flask only.
+api = Api(app)
+
+class CreateUrlInput(Resource):
+  def get(self):
+    return jsonify_csv_input(1)
+
+class DoNothing(Resource):
+  def get(self, word):
+    return do_nothing(word)
+
+class PrComment(Resource):
+  def get(self, prcomment):
+    return get_comment_polarity(prcomment)
+
+api.add_resource(CreateUrlInput, '/')
+api.add_resource(DoNothing, '/api/donothing/<string:word>')
+api.add_resource(PrComment, '/api/prcomment/<string:prcomment>')
+
+def run():
+  app.run(host='0.0.0.0',port=7210)
+
+t = Thread(target=run)
+t.start()
---
+++ process.py
@@ -0,0 +1,22 @@
+import nltk
+import pandas as pd
+
+nltk.download('punkt')
+from textblob import TextBlob
+
+def do_nothing(word):
+  return word
+
+def jsonify_csv_input(n):
+  comments = pd.read_csv("dummy_comments.csv").drop(columns='createdAt').head(n)
+  return comments.body.to_json()
+
+def get_file_polarity():
+  comments = pd.read_csv("dummy_comments.csv").drop(columns='createdAt').head(5)
+  # for each row, run textblob on the column body and write the result   in a new column called "polarity"
+  comments['polarity'] = comments.body.apply(
+    lambda x: TextBlob(x).sentiment.polarity)
+  return comments.to_json()
+
+def get_comment_polarity(comment):
+  return TextBlob(comment).sentiment.polarity
